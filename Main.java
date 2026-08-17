0a3f3b3f284e948fd75e64cbe35ba04592c1aec0
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:25:09 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        DatabaseConnection db = new DatabaseConnection();
+        db.connect();
 
+        Employee employee = new Employee(1, "Ajay", 50000);
+
+        Payroll payroll = new Payroll();
+        payroll.generateSalary(employee);
+    }
+}
