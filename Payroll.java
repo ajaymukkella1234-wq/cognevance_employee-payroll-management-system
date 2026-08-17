f0b07d0aaa43e9743dfc314fedc90c4bfcbc4651
ajay123 <ajaymukkella1234@gmail.com>
2026-08-17 22:24:34 +0530

    Update Payroll.java

--- Payroll.java
+++ Payroll.java
@@ -1,1 +1,8 @@
-
+// Payroll.java
+class Payroll {
+    public void generateSalary(Employee employee) {
+        System.out.println("Employee ID: " + employee.id);
+        System.out.println("Employee Name: " + employee.name);
+        System.out.println("Salary: " + employee.salary);
+    }
+}
